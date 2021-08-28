3859b99f4d7d909cbd151ceec6dfed5a62652b65
Erwin Ruiz <88996407+erwinruiz@users.noreply.github.com>
2021-08-27 19:07:07 -0700

    Remove manipulation of CSS on JavaScript file

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ button.addEventListener("click", () => {
 
   const reg =
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!enteredEmail.match(reg)) {
-    errorMessage.style["display"] = "initial";
-    input.style["border"] = "1px solid var(--SoftRed)";
-    return;
-  }
 
-  errorMessage.style["display"] = "none";
-  input.style["border"] = "1px solid var(--DesaturatedRed)";
+  if (!enteredEmail.match(reg)) {
+    errorMessage.classList.remove("hide");
+    input.classList.add("input_error");
+  } else {
+    errorMessage.classList.add("hide");
+    input.classList.remove("input_error");
+  }
 });
